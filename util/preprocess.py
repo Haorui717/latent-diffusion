602fc3ddd18bbb70937b9b84ee9f498a6fe4e1c8
Haorui <384883654@qq.com>
2023-07-11 15:53:56 -0400

    rename the vqf8 config file. Edit the preprocess file.

--- util/preprocess.py
+++ util/preprocess.py
@@ -2,19 +2,55 @@ import os
 import shutil
 
 
-mask_paths = [
-    '/home/zongwei/haorui/ccvl15/haorui/datasets/PolypGen2021_MultiCenterData_v3/data_C1/masks_C1',
-    '/home/zongwei/haorui/ccvl15/haorui/datasets/PolypGen2021_MultiCenterData_v3/data_C2/masks_C2',
-    '/home/zongwei/haorui/ccvl15/haorui/datasets/PolypGen2021_MultiCenterData_v3/data_C3/masks_C3',
-    '/home/zongwei/haorui/ccvl15/haorui/datasets/PolypGen2021_MultiCenterData_v3/data_C4/masks_C4',
-    '/home/zongwei/haorui/ccvl15/haorui/datasets/PolypGen2021_MultiCenterData_v3/data_C5/masks_C5',
+
+def load_filepaths(dir):
+    filepaths = []
+    for filename in os.listdir(dir):
+        filepaths.append(os.path.join(dir, filename))
+    return filepaths
+
+dirs = [
+    '/home/zongwei/haorui/ccvl15/haorui/datasets/PolypGen2021_MultiCenterData_v3/data_C1/images_C1',
+    '/home/zongwei/haorui/ccvl15/haorui/datasets/PolypGen2021_MultiCenterData_v3/data_C2/images_C2',
+    '/home/zongwei/haorui/ccvl15/haorui/datasets/PolypGen2021_MultiCenterData_v3/data_C3/images_C3',
+    '/home/zongwei/haorui/ccvl15/haorui/datasets/PolypGen2021_MultiCenterData_v3/data_C4/images_C4',
+    '/home/zongwei/haorui/ccvl15/haorui/datasets/PolypGen2021_MultiCenterData_v3/data_C5/images_C5',
+    '/home/zongwei/haorui/ccvl15/haorui/datasets/PolypGen2021_MultiCenterData_v3/data_C6/images_C6',
+    '/home/zongwei/haorui/ccvl15/haorui/datasets/PolypGen2021_MultiCenterData_v3/imagesAll_positive',
 ]
 
-# images name ends with <imagename>.jpg/.png but masks name ends with <imagename>_mask.jpg/.png
-# so we need to rename the masks by removing the '_mask' part
+all_images = []
 
-for mask_path in mask_paths:
-    for filename in os.listdir(mask_path):
-        if '_mask' in filename:
-            os.rename(os.path.join(mask_path, filename), os.path.join(mask_path, filename.replace('_mask', '')))
+for dir in dirs:
+    all_images += load_filepaths(dir)
+
+print(len(all_images))
+
+# randomly select 10% images as validation set
+
+import random
+
+random.shuffle(all_images)
+
+val_images = all_images[:int(len(all_images) * 0.1)]
+train_images = all_images[int(len(all_images) * 0.1):]
+
+# sort three lists and save them to txt files
+val_images.sort()
+train_images.sort()
+all_images.sort()
+
+with open('PolyGEN_p+n_val_images.txt', 'w') as f:
+    for image in val_images:
+        f.write(image + '\n')
+
+with open('PolyGEN_p+n_train_images.txt', 'w') as f:
+    for image in train_images:
+        f.write(image + '\n')
+
+with open('PolyGEN_p+n_all_images.txt', 'w') as f:
+    for image in all_images:
+        f.write(image + '\n')
+
+
 
